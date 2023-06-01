d9f949acd9f6c86ca5cbbec7f31cb203a2128c8a
neel-1905 <neelnarwadkar@gmail.com>
2023-06-01 13:08:58 +0530

    added post request for register and login

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,19 +1,78 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import loginStyles from "../Styles/login.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../Redux/authSlice";
 
 const Login = () => {
+  const [details, setDetails] = useState({
+    username: null,
+    password: null,
+  });
+
+  const { user } = useSelector((state) => state.auth);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setDetails({ ...details, [e.target.name]: e.target.value });
+    console.log(details);
+  };
+
+  const handleLogin = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: details.username,
+          password: details.password,
+        }),
+      });
+
+      const result = await res.json();
+
+      if (!result.isSuccess) {
+        throw new Error(result.error);
+      }
+
+      dispatch(login(result.username));
+      localStorage.setItem("token", result.token);
+      alert(`User Logged In! ${user}`);
+
+      navigate("/");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <>
       <div
         className={`d-flex justify-content-center align-items-center ${loginStyles.mainDiv}`}
       >
-        <form className={`col-sm-5 col-10 p-5 ${loginStyles.formDiv}`}>
+        <form
+          className={`col-sm-5 col-10 p-5 ${loginStyles.formDiv}`}
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            if (!details.username || !details.password) {
+              alert("Please fill all the details");
+            } else {
+              handleLogin();
+            }
+          }}
+        >
           <div className="mb-3">
             <label htmlFor="username" className="form-label">
               Username
             </label>
             <input
+              name="username"
+              onChange={handleChange}
               type="text"
               className={`form-control ${loginStyles.textInputs}`}
               id="username"
@@ -25,6 +84,8 @@ const Login = () => {
               Password
             </label>
             <input
+              onChange={handleChange}
+              name="password"
               type="password"
               className={`form-control ${loginStyles.textInputs}`}
               id="password"
--- frontend/src/Components/Register.jsx
+++ frontend/src/Components/Register.jsx
@@ -1,19 +1,76 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import loginStyles from "../Styles/login.module.css";
 
 const Register = () => {
+  const [details, setDetails] = useState({
+    username: null,
+    password: null,
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setDetails({ ...details, [e.target.name]: e.target.value });
+    console.log(details);
+  };
+
+  const handleRegister = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/users/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: details.username,
+          password: details.password,
+        }),
+      });
+
+      const result = await res.json();
+
+      if (!result.isSuccess) {
+        throw new Error(result.error);
+      }
+
+      alert("User Registered!");
+
+      navigate("/login");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <>
       <div
         className={`d-flex justify-content-center align-items-center ${loginStyles.mainDiv}`}
       >
-        <form className={`col-sm-5 col-10 p-5 ${loginStyles.formDiv}`}>
+        <form
+          className={`col-sm-5 col-10 p-5 ${loginStyles.formDiv}`}
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            if (details.password !== details.confirmPassword) {
+              alert("Passwords do not match");
+            } else if (
+              !details.username ||
+              !details.password ||
+              !details.confirmPassword
+            ) {
+              alert("Please fill all the details");
+            } else {
+              handleRegister();
+            }
+          }}
+        >
           <div className="mb-3">
             <label htmlFor="username" className="form-label">
               Username
             </label>
             <input
+              onChange={handleChange}
+              name="username"
               type="text"
               className={`form-control ${loginStyles.textInputs}`}
               id="username"
@@ -25,6 +82,8 @@ const Register = () => {
               Password
             </label>
             <input
+              name="password"
+              onChange={handleChange}
               type="password"
               className={`form-control ${loginStyles.textInputs}`}
               id="password"
@@ -36,13 +95,15 @@ const Register = () => {
               Confirm Password
             </label>
             <input
+              name="confirmPassword"
+              onChange={handleChange}
               type="password"
               className={`form-control ${loginStyles.textInputs}`}
               id="confirmPassword"
             />
           </div>
           <p>
-            <Link>Register</Link>
+            <Link to={`/login`}>Login</Link>
           </p>
           <button type="submit" className="btn btn-primary w-100">
             Register
